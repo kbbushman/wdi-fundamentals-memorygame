67a810c1304f14910edc960f7c27c912dc036e1d
Kenny B <KennyB@Kenneths-MacBook-Pro.local>
2016-08-26 13:38:09 -0700

    created boolean logic for checking card equality

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,11 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardThree) {
+	alert("You found a match");
+} else {
+	alert("Sorry, try again.")
+}
