b73e2ef872598656a269aa7cb6f80f23e0a69d94
Kenny B <KennyB@Kenneths-MacBook-Pro.local>
2016-08-27 23:51:28 -0700

    created function to add new cards to the gameboard.

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,7 +4,7 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
-
+/*
 if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else if (cardOne === cardThree) {
@@ -12,3 +12,16 @@ if (cardTwo === cardFour) {
 } else {
 	alert("Sorry, try again.")
 }
+*/
+
+var createBoard = function(num) {
+  for(var i = 1; i <= num; i += 1) {
+    var gameBoard = document.getElementById('game-board');
+    var newDiv = document.createElement('div');
+    newDiv.className = 'card';
+    gameBoard.appendChild(newDiv);
+    console.log('Card number ' + i + ' has been created successfully.');
+  }
+};
+
+createBoard(4);
