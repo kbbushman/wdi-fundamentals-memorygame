385c59ca70d4cf9f405d3dabed129902659528d6
Kenny B <KennyB@Kenneths-MacBook-Pro.local>
2016-08-30 22:22:12 -0700

    final game logic from unit 11 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,27 +1,47 @@
-console.log("JS file is connected to HTML! Woo!");
+console.log("Ready for orders, Captain...!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-/*
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-} else if (cardOne === cardThree) {
-	alert("You found a match");
-} else {
-	alert("Sorry, try again.")
-}
-*/
+var gameBoard = document.getElementById('game-board');
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var createBoard = function(num) {
-  for(var i = 1; i <= num; i += 1) {
-    var gameBoard = document.getElementById('game-board');
+createBoard();
+
+function createBoard() {
+  for(var i = 0; i < cards.length; i++) {
     var newDiv = document.createElement('div');
     newDiv.className = 'card';
+    newDiv.setAttribute('data-card', cards[i]);
+    newDiv.addEventListener('click', isTwoCards);
     gameBoard.appendChild(newDiv);
     console.log('Card number ' + i + ' has been created successfully.');
   }
 };
 
-createBoard(4);
+function isTwoCards() {
+  if (this.getAttribute('data-card') === 'king') {
+    this.innerHTML = '<img alt="King of Spades" src="http://k2sites.com/ga-wdi/images/king-spades.jpg" />';
+  }
+  if (this.getAttribute('data-card') === 'queen') {
+    this.innerHTML = '<img alt="Queen of Hearts" src="http://k2sites.com/ga-wdi/images/queen-hearts.jpg" />';
+  }
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+};
+
+function isMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+  clearBoard();
+};
+
+function clearBoard() {
+  gameBoard.innerHTML = '';
+  console.log('All clear...!');
+  createBoard();
+};
